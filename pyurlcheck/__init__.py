c0d2d7a20a544c1355ce12e27aaab21edb55553c
Jeff Kala <jeff.l.kala@gmail.com>
2021-07-17 21:22:50 -0500

    prep 0.1.1 release

--- pyurlcheck/__init__.py
+++ pyurlcheck/__init__.py
@@ -1,3 +1,3 @@
 """Pyurlcheck."""
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
--- tests/test_pyurlcheck.py
+++ tests/test_pyurlcheck.py
@@ -5,4 +5,4 @@ from pyurlcheck import __version__
 
 def test_version():
     """Simple version test."""
-    assert __version__ == "0.1.0"
+    assert __version__ == "0.1.1"
