9430e3917146e5fb8227c1c1a438388a33ec5a77
Jeff Kala <jeff.l.kala@gmail.com>
2021-12-02 21:09:25 -0600

    prep for 0.1.2 release

--- tests/test_pyurlcheck.py
+++ tests/test_pyurlcheck.py
@@ -1,8 +1,13 @@
 """Testing for pyurlcheck."""
+import os
 
+import toml
 from pyurlcheck import __version__
 
 
 def test_version():
     """Simple version test."""
-    assert __version__ == "0.1.1"
+    parent_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+    print(parent_path)
+    poetry_version = toml.load(f".{parent_path}pyproject.toml")["tool"]["poetry"]["version"]
+    assert __version__ == poetry_version
